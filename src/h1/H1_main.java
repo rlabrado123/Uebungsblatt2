6ae5a99335687e2cacdfdb0996a0a9ec97fe53b6
rlabrado123 <rlabrado@smail.uni-koeln.de>
2025-11-07 18:36:45 +0100

    Initial commit

--- src/h1/H1_main.java
+++ src/h1/H1_main.java
@@ -1,5 +1,45 @@
 package h1;
 
 public class H1_main {
+public static void main(String[] args) {
+	double guthaben = -100;
+	double monEingang = 200;
+	 int rating = -2;            // Beispielwert
+     boolean warnhinweis;
+     boolean negativ;
 
+     // Prüfen, ob das Guthaben negativ ist
+     if (guthaben < 0) {
+         negativ = true;
+     } else {
+         negativ = false;
+     }
+
+     // Rating berechnen
+     if (guthaben > 0) {
+         rating += 3;
+     } else if (guthaben == 0) {
+         rating += 2;
+     } else { // guthaben < 0
+         if (Math.abs(monEingang) >= Math.abs(guthaben)) {
+             rating += 1;
+         } else {
+             rating -= 1;
+         }
+     }
+
+     // Warnhinweis bestimmen
+     if (guthaben < 0 && Math.abs(monEingang) < Math.abs(guthaben) && rating < 0) {
+         warnhinweis = true;
+     } else {
+         warnhinweis = false;
+     }
+
+     // Ausgabe der Ergebnisse
+     System.out.println("Guthaben: " + guthaben);
+     System.out.println("Monatlicher Eingang: " + monEingang);
+     System.out.println("Negativ: " + negativ);
+     System.out.println("Rating: " + rating);
+     System.out.println("Warnhinweis: " + warnhinweis);
+}
 }
---
+++ src/h2/H2_main.java
@@ -0,0 +1,14 @@
+package h2;
+
+public class H2_main {
+public static void main(String[] args) {
+	int jahr = 2024;
+	boolean schalt = false;
+	if ((jahr % 4 == 0 && jahr % 100 != 0) || (jahr % 400 == 0)) {
+        schalt = true;
+    }else {
+    	schalt = false;
+    }
+System.out.println("Wert von schalt:"+schalt);
+}
+}
